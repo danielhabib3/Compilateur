ede64e703dfcb5439b0c0400cc3071c67c59d73a
danielhabib3 <danielhabib301@gmail.com>
2025-03-17 17:59:16 +0100

    .gitignore

--- ifcc-test-output/testfiles-WillNeverBeImplemented-19_affectation_a_la_declaration_2/asm-ifcc.s
+++
@@ -1,8 +0,0 @@
-.globl main
- main: 
-    pushq %rbp
-    movq %rsp, %rbp
-    movl $0, -4(%rbp)
-    movl -8(%rbp), %eax
-    popq %rbp
-    ret
--- ifcc-test-output/testfiles-WillNeverBeImplemented-7_return_negatif/asm-ifcc.s
+++
@@ -1,7 +0,0 @@
-.globl main
- main: 
-    pushq %rbp
-    movq %rsp, %rbp
-    movl $1, %eax
-    popq %rbp
-    ret
