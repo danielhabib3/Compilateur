e3b6dda4ca6f23d04854407d879295b165c1393e
LEVRARD Thomas <thomaslevrardw@gmail.com>
2025-04-17 18:50:24 +0200

    some_change_test

--- testfiles/TestsOK/Valid/continue/_continue_dans_if_dans_while.c
+++ testfiles/TestsOK/Valid/continue/_continue_dans_if_dans_while.c
@@ -1,11 +1,13 @@
 int main() {
     int i = 0;
+    int c = 0;
     while (i < 5) {
         if (i == 2) {
             i = i + 1;
             continue;
         }
+        c++;
         i = i + 1;
     }
-    return i;
+    return c;
 }
--- testfiles/TestsOK/Valid/continue/_continue_dans_while_inutile.c
+++ testfiles/TestsOK/Valid/continue/_continue_dans_while_inutile.c
@@ -1,8 +1,11 @@
 int main() {
     int i = 0;
+    int c = 0;
+    
     while (i < 5) {
         i = i + 1;
+        c++;
         continue;
     }
-    return i;
+    return c;
 }
