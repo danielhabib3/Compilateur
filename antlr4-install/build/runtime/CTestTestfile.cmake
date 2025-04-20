690e161a1a06c44ae025ddc88d10c0aec30f73b0
danielhabib3 <danielhabib301@gmail.com>
2025-04-20 15:41:21 +0200

    antlr4

---
+++ antlr4-install/build/runtime/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dhabib/antlr4-install/runtime
+# Build directory: /home/dhabib/antlr4-install/build/runtime
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/dhabib/antlr4-install/build/runtime/antlr4_tests[1]_include.cmake")
+subdirs("../_deps/googletest-build")
